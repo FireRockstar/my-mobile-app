320613d293508698b48c5b009a427589bac584fa
FireRockstar <160132360+FireRockstar@users.noreply.github.com>
2026-08-13 00:25:44 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TVRepairCRM from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TVRepairCRM />
+  </React.StrictMode>
+);
